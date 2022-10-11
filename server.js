8c04c059be7212804da1aa19bd281f9f877190c8
Te <worksingletonmcdaniel@gmail.com>
2022-10-10 22:51:10 -0700

    psuedo code in server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+// require express
+
+// inititalize a path variable
+
+// require `fs` (file system module)
+
+// intiallize app with express() module 
+
+
+
+// create middleware for parsing json
+
+
+
+
+
+// GET  route for homepage
+
+
+// GET  route for notes URL
+
+
+// GET request for retrieving previously stored notes
+
+
+// POST request to add note
+
+
+// DELETE request to delete note
+
+
+
+
+
