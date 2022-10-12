2684166a77895a598fe2a84c97ca1ae3373d1b81
Te <worksingletonmcdaniel@gmail.com>
2022-10-12 16:35:11 -0700

    adding to boilerplate to server js

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 // require express
+const express = require("express");
 
 // inititalize a path variable
+const path = require("path");
+const app = express();
+// register a path
+
+const PORT = process.env.PORT || 3001;
 
 // require `fs` (file system module)
 
-// intiallize app with express() module 
-
-
+// intiallize app with express() module
 
 // create middleware for parsing json
 
-
-
-
-
 // GET  route for homepage
-
 
 // GET  route for notes URL
 
-
 // GET request for retrieving previously stored notes
-
 
 // POST request to add note
 
-
 // DELETE request to delete note
-
-
-
-
-
