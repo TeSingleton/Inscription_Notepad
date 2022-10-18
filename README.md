28263778395857b7dcbdaeafc58dcc5855d98434
Te <worksingletonmcdaniel@gmail.com>
2022-10-17 18:14:35 -0700

    adding/updating readme

--- README.md
+++ README.md
@@ -1,20 +1,55 @@
-# Inscription_Notepad
-A simple note taking app to keep track of task that require attention
 
+  
+# Inscription Notepad
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)](https://opensource.org/licenses/MIT)
 
+## Description
 
+    A note Taking app built using express.js for the back end .
 
-GIVEN a note-taking application
-WHEN I open the Note Taker
-THEN I am presented with a landing page with a link to a notes page
-WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-WHEN I enter a new note title and the note’s text
-THEN a Save icon appears in the navigation at the top of the page
-WHEN I click on the Save icon
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-WHEN I click on an existing note in the list in the left-hand column
-THEN that note appears in the right-hand column
-WHEN I click on the Write icon in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+## Table of Contents (clickable)
+     
+ * [Description](#description)
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [Contributing](#contributing)
+ * [Tests](#tests)
+ * [Questions?](#questions)
+    
+  ## Installation 
+    Clone down the repository or visit the deloyed URL to use the app. 
+
+  ## Usage
+
+    Use app to take notes
+
+  ## License
+    
+    Copyright 2022 TeSingleton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   
+
+  ## Contributing
+
+     All work is welcome . Please create an issue for any big changes
+
+  ## Tests
+
+    
+
+  ## Questions?
+
+    Any inquiries , feel free to reach out
+
+  GitHub : <a href="https://github.com/TeSingleton">TeSingleton</a>
+
+  Email:  <a href="mailto:worksingletonmcdaniel@gmail.com">worksingletonmcdaniel@gmail.com</a>
+
+ 
+    
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-var storedNotes = require('./db/db.json');
-const { Server } = require('http');
-const { prototype } = require('events');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+var storedNotes = require("./db/db.json");
+const { Server } = require("http");
+const { prototype } = require("events");
 
 const app = express();
-// PORT variable with the node env variable 
+// PORT variable with the node env variable
 const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
@@ -15,95 +15,89 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // using the static files from the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET route for /notes URL
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // GET request for grabbing stored notes
-app.get('/api/notes', (req, res) => {
-    res.json(storedNotes);
+app.get("/api/notes", (req, res) => {
+  res.json(storedNotes);
 });
 
 // POST request to add new note
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a new note.`);
-// destructured note object as a variable
-    const {title, text} = req.body
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            // using the identifier function within the newNote object
-            id: uuidv4()
-        };
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a new note.`);
+  // destructured note object as a variable
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      // using the identifier function within the newNote object
+      id: uuidv4(),
+    };
 
-        // pushing to newNote Array
-        storedNotes.push(newNote);
+    // pushing to newNote Array
+    storedNotes.push(newNote);
 
-        
-        fs.writeFile(`./db/db.json`, JSON.stringify(storedNotes, null, 2), (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `"${newNote.title}" has been added to JSON file`
-            )
-      );
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in writing note');
-    }
+    fs.writeFile(`./db/db.json`, JSON.stringify(storedNotes, null, 2), (err) =>
+      err
+        ? console.error(err)
+        : console.log(`"${newNote.title}" has been added to JSON file`)
+    );
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in writing note");
+  }
 });
 
 // DELETE request to delete note
-app.delete('/api/notes/:id', (req, res) => {
-    console.info(`${req.method} request received to delete a note.`);
-    
-    const id = req.params.id;
-    if (id) {
+app.delete("/api/notes/:id", (req, res) => {
+  console.info(`${req.method} request received to delete a note.`);
 
-        storedNotes = storedNotes.filter(item => item.id !== id);
-        
-        fs.writeFile(`./db/db.json`, JSON.stringify(storedNotes, null, 2), (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Note with id ${id} has been deleted from JSON file`
-            )
-      );
-  
-      const response = {
-        status: 'success',
-        id: id
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in deleting note');
-    }
+  const id = req.params.id;
+  if (id) {
+    storedNotes = storedNotes.filter((item) => item.id !== id);
+
+    fs.writeFile(`./db/db.json`, JSON.stringify(storedNotes, null, 2), (err) =>
+      err
+        ? console.error(err)
+        : console.log(`Note with id ${id} has been deleted from JSON file`)
+    );
+
+    const response = {
+      status: "success",
+      id: id,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in deleting note");
+  }
 });
 
 // GET route for homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-// Listen to PORT 
+// Listen to PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} `)
 );
